98cad4a1cc64953380f30f7c9adef0dbd2c8bdde
khadija garba <naura1835@gmail.com>
2022-06-18 19:27:33 +0100

    update product data

--- src/data/products.data.js
+++ src/data/products.data.js
@@ -8,23 +8,24 @@ export const product = {
   images: [
     {
       imageURL:
-        "https://images.pexels.com/photos/7919142/pexels-photo-7919142.jpeg",
-      altText: "cactus in a pot",
+        "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
+      altText: "a pair of nike shoes levitating in the air",
     },
     {
       imageURL:
-        "https://images.pexels.com/photos/7159915/pexels-photo-7159915.jpeg",
-      altText: "cacti in different pots",
+        "https://images.pexels.com/photos/2529147/pexels-photo-2529147.jpeg",
+      altText: "side view of one nike shoe",
     },
     {
       imageURL:
-        "https://images.pexels.com/photos/3075987/pexels-photo-3075987.jpeg",
-      altText: "",
+        "https://images.pexels.com/photos/2529146/pexels-photo-2529146.jpeg",
+      altText:
+        "back view of nike shoe with a bright yellow shin, black sole and blush and blue top part",
     },
     {
       imageURL:
-        "https://images.pexels.com/photos/9824876/pexels-photo-9824876.jpeg",
-      altText: "",
+        "https://images.pexels.com/photos/6540973/pexels-photo-6540973.jpeg",
+      altText: "a nike white shoe with bright red color inside",
     },
   ],
 };
